f052c31aeab3367a7f6b927559beec1850e9a37c
blackwires <a.albertini2b@gmail.com>
2014-03-12 10:35:28 +0100

    Add some rackets.

--- Assets/Base/Scripts/Datas/SingleRacketDatas.cs
+++ Assets/Base/Scripts/Datas/SingleRacketDatas.cs
@@ -7,5 +7,5 @@ public class SingleRacketDatas
     public string m_Name = "Empty";
     public Sprite m_Sprite = null;
     public float m_Width = 1.0f;
-    public float m_Speed = 1.0f;
+    public float m_Speed = 4.0f;
 }
